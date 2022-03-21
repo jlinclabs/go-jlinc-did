8ce724f7182f1c44bc2af2ffcb6b7cdacbc1edf9
Victor Grey <victorgrey@users.noreply.github.com>
2022-03-21 12:39:05 -0700

    Added some utility methods.

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/jlinclabs/go-jlinc-did
 
 go 1.17
 
-require golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd
+require (
+	github.com/shengdoushi/base58 v1.0.0
+	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd
+)
 
 require golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
---
+++ utils.go
@@ -0,0 +1,47 @@
+package did
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/base64"
+
+	"github.com/shengdoushi/base58"
+)
+
+func getHash(j string) []byte {
+	h := sha256.New()
+	h.Write([]byte(j))
+	return h.Sum(nil)
+}
+
+func getByteHash(j []byte) []byte {
+	h := sha256.New()
+	h.Write(j)
+	return h.Sum(nil)
+}
+
+func b64Decode(s string) []byte {
+	decoded, _ := base64.RawURLEncoding.DecodeString(s)
+	return decoded
+}
+
+func b64Encode(h []byte) string {
+	var buf bytes.Buffer
+	encoder := base64.NewEncoder(base64.RawURLEncoding, &buf)
+	encoder.Write(h)
+	encoder.Close()
+	return buf.String()
+}
+
+func b58Decode(s string) []byte {
+	decoded, _ := base58.Decode(s, base58.BitcoinAlphabet)
+	return decoded
+}
+
+func b58Encode(h []byte) string {
+	return base58.Encode(h, base58.BitcoinAlphabet)
+}
+
+func b58tob64(s string) string {
+	return b64Encode(b58Decode(s))
+}
