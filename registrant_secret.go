9d36464a20df1bae8fe035738dcde9135edb705c
Victor Grey <victorgrey@users.noreply.github.com>
2022-03-25 17:34:17 -0700

    Fixed the way we create and return the registrant secret.

--- registrant_secret.go
+++ registrant_secret.go
@@ -7,15 +7,16 @@ import (
 	"golang.org/x/crypto/nacl/box"
 )
 
-func createRegistrantSecret(serverKey string, ent entity) (string, error) {
+func createRegistrantSecret(serverKey string, ent entity) (string, string, error) {
 	registerSecret := ent.RegistrationSecret
 
 	var nonce [24]byte
 	if _, err := io.ReadFull(rand.Reader, nonce[:]); err != nil {
-		return "", err
+		return "", "", err
 	}
 
-	registrantSecret := box.Seal(nonce[:], []byte(registerSecret), &nonce, (*[32]byte)(b64Decode(serverKey)), (*[32]byte)(b64Decode(ent.EncryptingPrivateKey)))
+	var out []byte
+	registrantSecret := box.Seal(out, []byte(registerSecret), &nonce, (*[32]byte)(b64Decode(serverKey)), (*[32]byte)(b64Decode(ent.EncryptingPrivateKey)))
 
-	return b64Encode(registrantSecret), nil
+	return b64Encode(registrantSecret), b64Encode(nonce[:]), nil
 }
